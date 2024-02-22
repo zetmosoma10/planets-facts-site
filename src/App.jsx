b4a684141d1d1bbf26d3fa714f0aaeb150881061
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-02-22 19:23:22 +0200

    add global styles and basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>App rendered!</h1>
-      <p>
+      <p className="body-text">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam rerum,
         illo nemo dicta eum pariatur velit quidem ipsum tenetur hic.
       </p>

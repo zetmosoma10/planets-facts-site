3dd0007dbc0162f66dd4d2cfc9d74f4960b64d0b
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-02-22 18:02:08 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>App rendered!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam rerum,
+        illo nemo dicta eum pariatur velit quidem ipsum tenetur hic.
+      </p>
+    </div>
+  );
+}
+
+export default App;

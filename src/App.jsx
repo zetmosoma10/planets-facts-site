f862ada6e406c27e3df005bcdf97e6464d2b009a
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-02-23 15:45:02 +0200

    add all components together

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { Feature, SubNavbar, Navbar, Hero } from "./components/index";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Hero />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/components/mainNav/Navbar.jsx
+++ src/components/mainNav/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import navStyles from "./mainNav.module.css";
 
 function Navbar() {
@@ -36,23 +37,39 @@ function Navbar() {
     },
   ];
 
+  const [toggler, setToggler] = useState(true);
+
+  const handleToggle = () => {
+    setToggler((prevToggler) => !prevToggler);
+  };
+
   return (
-    <>
+    <header className="container">
       <nav className={navStyles.main_nav}>
         <h2 className={navStyles.logo}>the planets</h2>
-        <img className={navStyles.toggler} src="assets/icon-hamburger.svg" />
+        <img
+          onClick={handleToggle}
+          className={`${navStyles.toggler} ${
+            toggler ? null : navStyles.active
+          }`}
+          src="assets/icon-hamburger.svg"
+        />
         <ul className={navStyles.nav_list_items}>
           {planets.map((planet) => (
-            <li className={navStyles.nav_item}>
+            <li className={navStyles.nav_item} key={planet.name}>
               <a href={planet.path}>{planet.name}</a>
             </li>
           ))}
         </ul>
       </nav>
-      <div className={navStyles.mobile_links}>
-        <ul className={navStyles.mobile_list_items}>
+      <div
+        className={`${navStyles.mobile_links} ${
+          toggler ? navStyles.show : null
+        }`}
+      >
+        <ul className={`${navStyles.mobile_list_items} container`}>
           {planets.map((planet) => (
-            <li className={navStyles.mobile_item}>
+            <li className={navStyles.mobile_item} key={planet.name}>
               <div className={navStyles.name_wrapper}>
                 <div className={navStyles.color}></div>
                 <a href={planet.path}>{planet.name}</a>
@@ -62,7 +79,7 @@ function Navbar() {
           ))}
         </ul>
       </div>
-    </>
+    </header>
   );
 }
 
